af966f49a4db187d5507a06c28be518b669caf2b
Adi115 <madnan260@gmail.com>
2023-02-21 13:11:07 +0500

    update token page

--- app/Views/themes/website_template/token.php
+++ app/Views/themes/website_template/token.php
@@ -5,16 +5,13 @@
       </div>
     </div>
     <div class="row justify-content-center">
-
       <h1 style="text-align:center">DiamondNXT <br> TOKEN SALE</h1>
-
       <p style="text-align:center; color:#D8D8D8;">1 DNXT = 1 MATIC</p>
       <div class="col-sm-6 col-md-6" style="margin-top:30px">
         <?php if (isset($message)) { ?>
           <div class="alert alert-success alert-dismissible">
             <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
             <strong><?php echo display('Success'); ?>!</strong> <?php echo esc($message); ?>
-
           </div>
         <?php } ?>
         <?php if (isset($exception)) { ?>
@@ -29,15 +26,9 @@
             <img src="<?php echo base_url() . '/public/assets/images/icons/matic.png' ?>" ; width="20" />
             <span style="font-size: 12px; color: #808080; padding-left: 7px;">MATIC</span>
           </span>
-          <input placeholder="0.00" aria-label="0.00" aria-describedby="basic-addon1" type="number" class="form-control" />
+          <input id="maticValue" value="1" placeholder="0.00" aria-label="0.00" aria-describedby="basic-addon1" type="number" class="form-control" />
         </div>
-        <div style="
-          height: 40px;
-          width: 40px;
-          position: relative;
-          margin: -18px auto;
-          border: 4px solid rgb(26 26 26);
-          z-index: 2;">
+        <div class="diamondLogo">
           <center><img src="<?php echo base_url() . '/public/assets/images/icons/shift.png' ?>" ; width="40" /></center>
         </div>
         <div class="mb-3 input-group">
@@ -45,7 +36,7 @@
             <img src="<?php echo base_url() . '/public/assets/images/icons/dnxt.png' ?>" ; width="20" />
             <span style="font-size: 12px; color: #808080; padding-left: 7px;">DNXT</span>
           </span>
-          <input placeholder="0.00" aria-label="0.00" aria-describedby="basic-addon1" type="number" class="form-control" style="" />
+          <input id="dnxtValue" onkeyup="calculateDNXT(this.value)" value="25" placeholder="0.00" aria-label="0.00" aria-describedby="basic-addon1" type="number" class="form-control" style="" />
         </div>
         <!-- Submit -->
         <center>
@@ -58,3 +49,28 @@
   </div>
   </div>
 </section>
+
+<style>
+  .diamondLogo {
+    height: 40px;
+    width: 40px;
+    position: relative;
+    margin: -18px auto;
+    border: 4px solid rgb(26 26 26);
+    z-index: 2;
+  }
+
+  input[type=number]:focus {
+    background-color: #1A1A1A;
+    color: white;
+  }
+</style>
+
+<script>
+  const input1 = document.getElementById('maticValue');
+  const input2 = document.getElementById('dnxtValue');
+
+  function calculateDNXT(ish) {
+    input1.value = ish * 0.04;
+  }
+</script>
